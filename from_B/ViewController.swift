0955c07a3f12c4ed5b3f0ec1e860ba381be59878
AnnaSemichaeva <tdhnss@gmail.com>
2022-05-31 16:14:05 +0300

    Initial commit

---
+++ from_B/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  from_B
+//
+//  Created by macuser on 5/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
